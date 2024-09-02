79ef884da43bd9303ef36f334bccb51f710fefbd
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-02 22:16:19 +0200

    add database connection with pg

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express"
 import bodyParser from "body-parser"
 import dotenv from 'dotenv'
+import pg from 'pg'
 
 dotenv.config()
+// Disable SSL verification globally (for development purposes only)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 const app = express()
 const port = process.env.PORT || 3000
 
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+db.connect().catch(err => {
+  console.error('Connection error', err.stack);
+})
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
