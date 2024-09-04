0cdfd15e560745e8e8fbb4f649d73c375ccfbb41
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-04 02:27:30 +0200

    create post /add in index.js

--- index.js
+++ index.js
@@ -75,6 +75,40 @@ app.get("/", async (req, res) => {
   }
 })
 
+app.post("/add", async (req, res) => {
+  try {
+    let country = req.body.country
+    //check the country code from countries table/ loosely match the country in case of incomplete name or upper/lowercase with LIKE, wildcards
+    let response = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%'|| $1 ||'%'", [country.toLowerCase()])
+    // If country_code is not found, no rows were returned, handle the error
+    if (response.rowCount === 0) {
+      throw new Error('Country does not exist, try again')
+    }
+
+    //add countryCode to visited_countries table
+    let countryCode = response.rows[0].country_code
+    await db.query('INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)',[countryCode, currentUserId])
+
+    //in visited_countries table, country_code is set to unique, it'll show an error 'Duplicate key value violates unique constraint' with error.code === '23505'
+    //if countryCode is a new country and get posted, reload the countries with get / after update the country
+    res.redirect("/")
+
+  } catch (error) {
+    console.error(error.message)
+
+    //pass all of the data including error message
+    const usersResult = await checkUsers()
+    const countries = await checkVisited()
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      users: usersResult,
+      color: currentUser.color,
+      error: error.message
+    })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
